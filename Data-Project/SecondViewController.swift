32ad7f43c34261d7e1ca7e31606e4d0d420cbb4e
Lubov <lb.librab14@gmail.com>
2022-05-11 23:08:31 +0300

    init

---
+++ Data-Project/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  Data-Project
+//
+//  Created by Lubov Burbygina on 11.05.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    var name: String?
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var buttonBack: UIButton!
+    
+    @IBAction func backButton(_ sender: UIButton) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        buttonBack.layer.cornerRadius = 30
+        
+        guard let name = self.name else { return }
+        label.text = "Hello, \(name)"
+        
+    }
+}
--- Data-Project/ViewController.swift
+++ Data-Project/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var nameTF: UITextField!
+    
+    
+    @IBOutlet weak var phoneTF: UITextField!
+    
+    
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    
+    @IBOutlet weak var buttonNext: UIButton!
+    
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "dataSegue", sender: nil)
+    }
+    
+    @IBAction func unwindSegueToMainScreen(segue: UIStoryboardSegue) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let dvc = segue.destination as? SecondViewController else { return }
+        dvc.name = nameTF.text
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        buttonNext.layer.cornerRadius = 30
+        
     }
-
-
 }
 
